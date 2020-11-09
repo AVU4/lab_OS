f7f86a35d9aecc8bbfd824635176c3477cb19fc0
AVU4 <vai00@184.education>
2020-11-09 12:43:10 +0300

    Made first point

--- lab.c
+++ lab.c
@@ -5,61 +5,67 @@
 
 #define D 78
 
-struct pair{
-	int file;
-	FILE* address;
-};
+typedef struct{
+	long start_address;
+	FILE* file;
+} thread_info;
+
+const long size_for_thread = 197 * 1024 * 1024 / D;
 
 void* write(void* data){
-	struct pair* current_data = (struct pair*) data;
-	fgets(current_data->address, 1, current_data->file);
+	thread_info* current_data = (thread_info*) data;
+	fgets(0xA35E1090 + size_for_thread * current_data->start_address , 4, current_data->file);
 	pthread_exit(0);
 }
 
-void fill_space(int* address){
-		
+void fill_space(){
+	
+	printf("Before allocation");
+	getchar();
 
-	struct pair* data;
+	int* address = mmap(0xA35E1090, 197 * 1024 * 1024,
+			PROT_READ | PROT_WRITE | PROT_EXEC,
+			MAP_ANONYMOUS | MAP_PRIVATE,
+			0,
+			0);
+	
+	printf("After allocation");
+	getchar();
+
+	thread_info informations[D];
 	pthread_t threads[D];
 	size_t i;
 	
+	const char* filename = "/dev/urandom";
+	FILE* file_random = fopen(filename, "r");
 
-	data->file = fopen("/dev/urandom", "r");
-
-	data->address = address;
+	if (file_random == NULL){
+		fprintf(stderr, "Couldn't open %s\n", "/dev/urandom");
+		exit(1);
+	}
 
 	for (i = 0; i < D; i ++){
-		pthread_create(&threads[i], NULL, write, data);
+		informations[i].start_address = i;
+		informations[i].file = file_random;
 	}
+
+	for (i = 0; i < D; i ++){
+		pthread_create(&threads[i], NULL, write, &informations[i]);
+	}
+	
+	printf("After fill data");
+	getchar();
+
+	munmap(address, 197 * 1024 * 1024);
+	fclose(file_random);
+	printf("After deallocation");
+	getchar();
 
 }
 
 
 int main(void){
-	printf("Before allocation");
-	int* address = 0xA35E1090;
-	getchar();
-	int* status =  mmap (address, 197 * 1024 * 1024,
-	   PROT_READ | PROT_WRITE | PROT_EXEC,
-	   MAP_ANONYMOUS | MAP_PRIVATE,
-	   0,
-	   0);
-
-	if (status == 0) {
-		printf("Error");
-	}
-	printf("After allocation");
-	getchar();
-
-	fill_space(status);
-
-	printf("After fill space");
-	getchar();
-
-	free(status);
-	printf("After deallocation");
-
-	getchar();
+	fill_space();
 
 
 }
